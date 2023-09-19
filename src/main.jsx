679082b5cf85c1f6cd5a1e3fc11caf79b8386744
Habibmahaasin <habibirf23@gmail.com>
2023-09-20 03:55:40 +0700

    update - add archive section

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Notes from './pages/notes'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Notes from "./pages/notes";
+import "./index.css";
 import { ThemeProvider } from "@material-tailwind/react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Notes />,
+  },
+]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider>
-      <Notes />
+      <RouterProvider router={router} />
     </ThemeProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
--- src/pages/notes.jsx
+++ src/pages/notes.jsx
@@ -1,7 +1,14 @@
 import MainLayout from "../components/Layouts/MainLayout";
 import FormNotes from "../components/Fragments/FormNotes";
 import NoteList from "../components/Fragments/NoteList";
-import AccordionList from "../components/Elements/Accordion"
+import AccordionList from "../components/Elements/Accordion";
+import {
+  Tabs,
+  TabsHeader,
+  TabsBody,
+  Tab,
+  TabPanel,
+} from "@material-tailwind/react";
 
 const list_notes = [
   {
@@ -9,9 +16,23 @@ const list_notes = [
     title: "Babel",
     body: "Babel merupakan tools open-source yang digunakan untuk mengubah sintaks ECMAScript 2015+ menjadi sintaks yang didukung oleh JavaScript engine versi lama. Babel sering dipakai ketika kita menggunakan sintaks terbaru termasuk sintaks JSX.",
     archived: false,
-    createdAt: '2022-04-14T04:27:34.572Z'
+    createdAt: "2022-04-14T04:27:34.572Z",
   },
-]
+  {
+    id: 2,
+    title: "Babel Archived",
+    body: "Babel merupakan tools open-source yang digunakan untuk mengubah sintaks ECMAScript 2015+ menjadi sintaks yang didukung oleh JavaScript engine versi lama. Babel sering dipakai ketika kita menggunakan sintaks terbaru termasuk sintaks JSX.",
+    archived: true,
+    createdAt: "2022-04-14T04:27:34.572Z",
+  },
+  {
+    id: 3,
+    title: "Bael",
+    body: "Babel merupakan tools open-source yang digunakan untuk mengubah sintaks ECMAScript 2015+ menjadi sintaks yang didukung oleh JavaScript engine versi lama. Babel sering dipakai ketika kita menggunakan sintaks terbaru termasuk sintaks JSX.",
+    archived: false,
+    createdAt: "2022-04-14T04:27:34.572Z",
+  },
+];
 
 const Notes = () => {
   return (
@@ -19,18 +40,61 @@ const Notes = () => {
       <div className="min-h-[100px] p-2">
         <FormNotes />
       </div>
-      <div className="min-h-[100px] p-2">
-        <NoteList>
-          {list_notes.map((note) =>{
-            return (
-              <AccordionList title={note.title} key={note.id} id={note.id}>
-                <p className="text-sm text-gray-600">
-                  {note.body}
-                </p>
-              </AccordionList>
-            )
-          })}
-        </NoteList>
+
+      <div className="min-h-[100px] p-2 mt-9">
+        <Tabs value="notes" className="max-w-[40rem]">
+          <TabsHeader
+            className="bg-transparent"
+            indicatorProps={{
+              className: "bg-gray-900/10 shadow-none !text-gray-900",
+            }}
+          >
+            <Tab value="notes">Notes</Tab>
+            <Tab value="archive">Archive</Tab>
+          </TabsHeader>
+          <TabsBody
+            animate={{
+              initial: { y: 250 },
+              mount: { y: 0 },
+              unmount: { y: 250 },
+            }}
+          >
+            <TabPanel value="notes">
+              <NoteList>
+                {list_notes.map((note) => {
+                  if (note.archived === false) {
+                    return (
+                      <AccordionList
+                        title={note.title}
+                        key={note.id}
+                        id={note.id}
+                      >
+                        <p className="text-sm text-gray-600">{note.body}</p>
+                      </AccordionList>
+                    );
+                  }
+                })}
+              </NoteList>
+            </TabPanel>
+            <TabPanel value="archive">
+              <NoteList>
+                {list_notes.map((note) => {
+                  if (note.archived === true) {
+                    return (
+                      <AccordionList
+                        title={note.title}
+                        key={note.id}
+                        id={note.id}
+                      >
+                        <p className="text-sm text-gray-600">{note.body}</p>
+                      </AccordionList>
+                    );
+                  }
+                })}
+              </NoteList>
+            </TabPanel>
+          </TabsBody>
+        </Tabs>
       </div>
     </MainLayout>
   );
